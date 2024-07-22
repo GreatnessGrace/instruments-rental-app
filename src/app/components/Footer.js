73ddeaaece17ba51bdcdebcb997218ce08b2d87b
GreatnessGrace <graceunstoppable7@gmail.com>
2024-07-22 16:58:05 +0530

    Footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-800 text-white p-4 mt-4">
+        <p>© 2024 DJ Sound Rental. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
